80bba91c6ca7e5cf989a7506baa419a91d441206
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-06-02 11:53:50 +0200

    Compenents + Correction Exo4

---
+++ src/components/Compteur.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref} from "vue"
+const count = ref(0)
+setInterval(function () {count.value++},1000)
+   function resetCompteur() {
+    count.value = 0;
+}
+</script>
+<template>
+<h1>Compteur : {{count}}</h1>
+  <div>
+    <button @click="resetCompteur()">Remettre à 0</button>
+    <br>
+    <input type="number" v-model="count">
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/DemoJwt.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import {ref} from "vue"
+  import * as jose from 'jose'
+  const jwt = ref("");
+  const decoded= ref("");
+  function decode(){
+    decoded.value = jose.decodeJwt(jwt.value);
+  }
+  function saveJwt(){
+    localStorage.setItem('jwt',jwt.value);
+  }
+  const savedJwt= ref("");
+  function loadSavedJwt() {
+    savedJwt.value= localStorage.getItem('jwt');
+  }
+</script>
+
+<template>
+  <input type="text" v-model="jwt" placeholder="insérer un JWT">
+  <button @click="decode()">Decoder</button>
+  <p>
+    {{decoded}}
+  </p>
+  <br><br>
+  <button @click="saveJwt()">Sauvegarder</button>
+  <p>
+    {{savedJwt}}
+  </p>
+</template>
+
+<style></style>

72fa4091572d2e6533fac779d97ab4be3ba56fd4
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-06-02 15:27:06 +0200

    change password class when badpassword

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,26 @@
   // import DemoVfor from "./components/DemoVfor.vue";
   // import DemoVif from "./components/DemoVif.vue";
   import {ref} from "vue";
+  const badPasword = ref(false)
 </script>
 
 <template>
-  <h1 class="text-danger">Hello World</h1>
+  <form @submit.prevent="badPasword=true">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label" :class="{'text-danger':badPasword}">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 </template>
 
 <style>

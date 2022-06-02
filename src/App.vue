6b5a2911bc54538c15071413eaecffd7ae264185
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-06-02 15:06:40 +0200

    composant Vif

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,12 @@
   // import Compteur from "./components/Compteur.vue";
   // import DemoJwt from "./components/DemoJwt.vue";
   // import DemoVfor from "./components/DemoVfor.vue";
+  // import DemoVif from "./components/DemoVif.vue";
   import {ref} from "vue";
-  const user = ref(null);
-  const connect = () => {
-    user.value = "Pochama";
-  };
-  const disconnect = () => {
-    user.value = null;
-  };  
 </script>
 
 <template>
-  <div v-if="user === null">
-    <h1>Hello Annonym</h1>
-    <button @click="connect">Se connecter</button>
-  </div>
-  <div v-else>
-    <h1>Hello {{ user }}</h1>
-    <button @click="disconnect">Se déconnecter</button>
-  </div>
+
 </template>
 
 <style>
---
+++ src/components/DemoVif.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import {ref} from "vue";
+  const user = ref(null);
+  const connect = () => {
+    user.value = "Pochama";
+  };
+  const disconnect = () => {
+    user.value = null;
+  };  
+</script>
+
+<template>
+  <div v-if="user === null">
+    <h1>Hello Annonym</h1>
+    <button @click="connect">Se connecter</button>
+  </div>
+  <div v-else>
+    <h1>Hello {{ user }}</h1>
+    <button @click="disconnect">Se déconnecter</button>
+  </div>
+</template>
+
+<style>
+
+</style>

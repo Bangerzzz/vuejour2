28673e2b60334e33162454e503a9fd20ad268349
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-06-02 14:50:46 +0200

    Connect/Disconnect

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
   // import Compteur from "./components/Compteur.vue";
   // import DemoJwt from "./components/DemoJwt.vue";
+  // import DemoVfor from "./components/DemoVfor.vue";
   import {ref} from "vue";
-import DemoVfor from "./components/DemoVfor.vue";
+  const user = ref(null);
+  const connect = () => {
+    user.value = "Pochama";
+  };
+  const disconnect = () => {
+    user.value = null;
+  };  
 </script>
 
 <template>
-  <DemoVfor/>
+  <div v-if="user === null">
+    <h1>Hello Annonym</h1>
+    <button @click="connect">Se connecter</button>
+  </div>
+  <div v-else>
+    <h1>Hello {{ user }}</h1>
+    <button @click="disconnect">Se déconnecter</button>
+  </div>
 </template>
 
 <style>

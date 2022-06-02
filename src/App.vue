7149bc2e0e2d230db8c954256afc1530d64435ff
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-06-02 14:05:39 +0200

    Exercice Tableau

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,40 @@
 <script setup>
-  import {ref} from "vue"
-  import * as jose from 'jose'
-
-  const count = ref(0)
-  const jwt = ref("");
-  const decoded= ref("");
-  let show = ref(false);
-  const token = ref("");
-
-  setInterval(function () {count.value++},1000)
-   function resetCompteur() {
-    count.value = 0;
-  }
-  function decode(){
-    decoded.value = jose.decodeJwt(jwt.value);
-    show.value=true;
-    addLocalStorage();
-  }
-  function addLocalStorage() {
-    // Si le token est vide
-    if ( !token.value ) {
-      localStorage.setItem('token', jwt.value);
-      token.value = localStorage.getItem('token');
+  // import Compteur from "./components/Compteur.vue";
+  // import DemoJwt from "./components/DemoJwt.vue";
+  import {ref} from "vue";
+  let id = ref(0);
+  const users = ref([
+    {id : id.value++, prenom: "Alice", nom: "Smith"},
+    {id : id.value++, prenom: "Bob", nom: "Stark"},
+    {id : id.value++, prenom: "Charlie", nom: "Doe"}
+  ]);
+  const prenom = ref("");
+  const nom = ref("");
+  function ajouterUser() {
+    if (prenom.value && nom.value) {
+      users.value.push({
+        id: id.value++,
+        prenom : prenom.value,
+        nom : nom.value
+      });
+      prenom.value = "";
+      nom.value = "";
     }
   }
-
 </script>
 
 <template>
-  <h1>Hello World!</h1>
-  <h1>Compteur : {{count}}</h1>
-  <div>
-    <button @click="resetCompteur()">Remettre à 0</button>
-    <br>
-    <input type="number" v-model="count">
-  </div>
+  <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user }}
+      </li>
+    </ul>
 
-  <h1>JWT exo3</h1>
-  <hr>
-
-  <input type="text" v-model="jwt" placeholder="insérer un JWT">
-  <button @click="decode()">Decoder</button>
-
-  <p v-if="show">
-    {{decoded}}
-  </p>
-
-  <p v-if="token">
-    {{ token }}
-  </p>
+    <form @submit.prevent="ajouterUser()">
+      <input type="text" placeholder="Prénom" v-model="prenom">
+      <input type="text" placeholder="Nom" v-model="nom">
+      <button>Ajouter</button>
+    </form>
 </template>
 
 <style>

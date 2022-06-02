4be3f2f8d54a4468e23c06c1f648a2003a7fd91e
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-06-02 10:28:44 +0200

    Exo1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {ref} from "vue"
+  const count = ref(0)
+
+  setInterval(function () {count.value++},1000)
+  console.log(count.value);
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+  <h1>Compteur : {{count}}</h1>
+</template>
+
+<style>
+
+</style>
